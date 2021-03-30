d92ced2959874c9172da7b766aa221c0f1a77735
mor bargig <morbargig@mors-MacBook-Pro.local>
2021-03-30 19:10:50 +0300

    by morbargig at Tue Mar 30 19:10:50 IDT 2021

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 # pull the current branch
 git pull origin (git branch --show-current)
 # git add,commit and push to your local branch // fit to jira
-git add . && git commit -m 'auto-commit' && git push origin (git branch --show-current)
+git add . && git commit -m (echo by (whoami) at (date) | string collect) && git push origin (git branch --show-current)
 # delete all branchs except the current branch 
 git branch | grep -v (git branch --show-current) | grep -v '^*' | xargs git branch -D # grep -v '^*'  and grep -v (git branch --show-current) doing the some => supposed to get the current branch
 # clone only development
